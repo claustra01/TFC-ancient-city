fbcef30c6d5e32beb9bb30ce7d3234bee19fb93a
claustra01 <shinya430.g@gmail.com>
2026-02-11 14:10:07 +0900

    fix: recipes

--- src/main/java/net/claustra01/tfcancientcity/TFCAncientCity.java
+++ src/main/java/net/claustra01/tfcancientcity/TFCAncientCity.java
@@ -5,10 +5,12 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -30,7 +32,19 @@ public class TFCAncientCity {
         PROCESSORS.register("tfc_block_replacement", () -> () -> TfcBlockReplacementProcessor.CODEC);
 
     public TFCAncientCity(IEventBus modEventBus) {
+        modEventBus.addListener(this::onBuildCreativeTabContents);
         ITEMS.register(modEventBus);
         PROCESSORS.register(modEventBus);
+    }
+
+    private void onBuildCreativeTabContents(BuildCreativeModeTabContentsEvent event) {
+        if (CreativeModeTabs.INGREDIENTS.equals(event.getTabKey())) {
+            event.accept(DEEPSLATE_LOOSE_ROCK_ITEM.get());
+            event.accept(DEEPSLATE_BRICK_ITEM.get());
+        } else if (CreativeModeTabs.BUILDING_BLOCKS.equals(event.getTabKey())) {
+            event.accept(DEEPSLATE_BRICK_ITEM.get());
+        } else if (CreativeModeTabs.NATURAL_BLOCKS.equals(event.getTabKey())) {
+            event.accept(DEEPSLATE_LOOSE_ROCK_ITEM.get());
+        }
     }
 }
